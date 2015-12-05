dbf36c39ed539d36d78ddd2148401a1c8b23e5c7
David Brown <db7g12@soton.ac.uk>
2015-12-05 21:31:05 +0000

    Testing commit

--- src/utils/RemoveDuplicates.java
+++ src/utils/RemoveDuplicates.java
@@ -20,6 +20,7 @@ public final class RemoveDuplicates {
 	 *            The array to have duplicates removed from
 	 */
 	public static void removeDuplicates(Object[] a) {
+		int test;
 		for (int i = 0; i < a.length; i++) {
 			Object next = a[i];
 			if (next == null) {
